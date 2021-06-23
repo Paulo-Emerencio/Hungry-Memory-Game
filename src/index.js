4814b0f4b09410b95954f0b2a067595c0b482d94
Paulo Emerencio <p.emerencio@gmail.com>
2021-06-23 13:06:00 -0300

    :hammer: Javascript practically done

--- src/index.js
+++ src/index.js
@@ -86,22 +86,54 @@ document.addEventListener('DOMContentLoaded', () => {
             img: 'src/images/donut.png',
         },
     ];
-    
+
     const grid = document.querySelector('.grid');
-    const cards = arrCards.concat(arrCards);
     const result = document.querySelector('#result');
+    const total = document.querySelector('#total');
+    const confirm = document.querySelector('#confirm');
+    const difficulty = document.querySelector('#difficulty');
+
+    const cardsSound = new Audio('src/audio/cards.mp3');
     const matchSound = new Audio('src/audio/popup.mp3');
     const victorySound = new Audio('src/audio/victory.mp3');
+
     let cardChosenName = [];
     let cardChosenId = [];
     let cardsWon = [];
+    let cards;
+    
+    confirm.addEventListener('click', startGame);
+    
+    function startGame(reset) {
+        arrCards.sort(() => 0.5 - Math.random());
+
+        const arrCardsFiltered = arrCards.filter((elem,index) =>
+            {if (index <= difficulty.value -1) return elem}
+        );
+
+        cards = arrCardsFiltered.concat(arrCardsFiltered);
+        cards.sort(() => 0.5 - Math.random());
+        
+        const allCardsImg = document.querySelectorAll('img');
+        allCardsImg.forEach(img => grid.removeChild(img));
+        cardChosenName = [];
+        cardChosenId = [];
+        cardsWon = [];
+        result.textContent = cardsWon.length;
+        total.textContent = '/' + difficulty.value;
+
+        if (difficulty.value == 6) grid.style.width = '436px';
+        if (difficulty.value == 12) grid.style.width = '654px';
+        if (difficulty.value == 21) grid.style.width = '763px';
+
+        if(reset)cardsSound.play();
+        createBoard();
+    }
 
     function createBoard() {
-        cards.sort(() => 0.5 - Math.random());
-
         for (let i = 0; i < cards.length; i++) {
             const blank = document.createElement('img');
-            blank.setAttribute('src', 'src/images/blank.png');
+            flipCardAnimation(blank, 'src/images/blank.png')
             blank.setAttribute('data-id', i);
             blank.addEventListener('click', flipCard);
             grid.appendChild(blank);
@@ -131,41 +163,32 @@ document.addEventListener('DOMContentLoaded', () => {
         setTimeout(() => {
             thisCard.setAttribute('src', srcValue);
             thisCard.classList.remove('turn');
-        },200);
+        }, 200);
     }
 
     function checkForMatch() {
-        const cardImg = document.querySelectorAll('img');
+        const image = document.querySelectorAll('img');
 
         if (cardChosenName[0] === cardChosenName[1]) {
-            cardImg[cardChosenId[0]].removeEventListener('click', flipCard);
-            cardImg[cardChosenId[1]].removeEventListener('click', flipCard);
+            image[cardChosenId[0]].removeEventListener('click', flipCard);
+            image[cardChosenId[1]].removeEventListener('click', flipCard);
             cardsWon.push(cardChosenName);
             result.textContent = cardsWon.length;
             if (cardsWon.length < cards.length / 2) matchSound.play();
         } else {
-            flipCardAnimation(cardImg[cardChosenId[0]], 'src/images/blank.png');
-            flipCardAnimation(cardImg[cardChosenId[1]], 'src/images/blank.png');
+            flipCardAnimation(image[cardChosenId[0]], 'src/images/blank.png');
+            flipCardAnimation(image[cardChosenId[1]], 'src/images/blank.png');
         }
         cardChosenName = [];
         cardChosenId = [];
 
         if (cardsWon.length === cards.length / 2) {
             victorySound.play();
+            total.textContent = '';
             result.textContent = 'Congratulations, you have won!';
-            setTimeout(resetGame, 5000, cardImg)
+            setTimeout(startGame, 5000, 'reset');
         }
     }
 
-    function resetGame(image) {
-        for (let i = 0; i < cards.length; i++) {
-            grid.removeChild(image[i]);
-        }
-        cardsWon = [];
-        result.textContent = cardsWon.length;
-
-        createBoard();
-    }
-
-    createBoard();
+    startGame();
 });
